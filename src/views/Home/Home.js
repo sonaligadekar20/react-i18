b6e38babfcea336184925c988667a0d663946ad9
sonaligadekar20 <sonaliygadekar@gmail.com>
2023-11-28 23:15:23 +0530

    initial commit

---
+++ src/views/Home/Home.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./Home.css"
+
+function Home() {
+
+    const usersCount = 15;
+    return (
+        <div>
+          <h1>Welcome!</h1>
+          <p>
+            We hope you enjoy learning React.
+          </p>
+
+          <h3>Have a great day!</h3>
+
+          <p>
+            {usersCount} users are learning in this session.
+          </p>
+        </div>
+    )
+}
+export default Home
